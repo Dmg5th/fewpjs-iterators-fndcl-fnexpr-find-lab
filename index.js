a36ded1728194862a90133e2aaecaa30aa45020e
Dmg5th <dmgould310@gmail.com>
2021-01-13 15:26:57 -0500

    commit tree 0692a331ef69f75a434e1cb7ff95e4a0c8e9ec27

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+] 
 
-function testFunc() {
-  return "hi"
+// function superbowlWin(array) {
+//   let findResult = array.find(o => o.result === "W")
+//   if (findResult) {
+//     return findResult.year
+//   } else {
+//     undefined
+//   }
+// }
+
+function superbowlWin(array) {
+  let findResult = array.find(o => {
+    return o.result === "W"
+  })
+   if (findResult) {
+        return findResult.year
+      } else {
+        undefined
+      }
 }
+
+
